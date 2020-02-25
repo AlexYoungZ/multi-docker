2dab23b07744c3a581669bb08093dc71c88ca39a
AlexYoungZ <zhangsiy2015@163.com>
2020-02-25 10:18:30 +0800

    add change

--- client/src/App.js
+++ client/src/App.js
@@ -13,9 +13,9 @@ class App extends Component {
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1
-              className="App-link"
+              className="App-title"
             >
-              Learn React
+              Fib Calculator
         </h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
